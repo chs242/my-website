bdec466e6d344e80f6e5a9c6a44145e0edc003be
Edward Stern <edwardstern242@gmail.com>
2019-12-16 15:24:17 +0000

    fixed body margin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueTypedJs from 'vue-typed-js'
+import VueTypedJs from 'vue-typed-js';
+import "./assets/Global.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueTypedJs)
